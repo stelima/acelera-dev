28e9d3d45ce38d845b32e660fada084687c6ef64
Araujo <stefanie.lima.araujo@accenture.com>
2019-07-13 15:52:01 -0300

    Template literal

---
+++ exercicio2.js
@@ -0,0 +1,24 @@
+// Use a sintaxe de template literal para mostrar cada entrada resultado do array failure. Cada entrada devera esta encapsulada dentro de um li conforme o exemplo
+const result = {
+ success: ["max-length", "no-amd", "prefer-arrow-functions"],
+ failure: ["no-var", "var-on-top", "linebreak"],
+ skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+ "use strict";
+
+ // change code below this line
+ const resultDisplayArray = arr.map((failure) => `<li class="text-warning">${failure}</li>`);
+
+ // change code above this line
+
+ return resultDisplayArray;
+}
+/**
+* makeList(result.failure) should return:
+* [ `<li class="text-warning">no-var</li>`,
+*   `<li class="text-warning">var-on-top</li>`,
+*   `<li class="text-warning">linebreak</li>` ]
+**/
+const resultDisplayArray = makeList(result.failure);
+console.log(resultDisplayArray);
